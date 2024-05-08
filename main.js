9b591c5adfb5f245c369bd6583b5fcc92b3bd46a
profnataguero <nfioretto@prof.educacao.sp.gov.br>
2024-05-08 19:48:28 -0300

    a

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuario=""
+let elemento = document.querySelector("#nome-usuario");
+
+while (nomeUsuario == ""){
+    nomeUsuario = prompt("Qual o seu nome?");
+}
+
+if (nomeUsuario == null){
+    elemento.textContent = 'seja muito bem vindo';
+}
+else{
+    elemento.textContent = nomeUsuario;
+}
